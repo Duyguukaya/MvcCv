9d57b6970f84956c232dfd86d31adb93cd320dcc
duygukayaa <dygkaya56@gmail.com>
2025-06-28 00:09:17 +0300

    vitrin paneli tamamlandı

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -3,15 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
 
 namespace MvcCv.Controllers
 {
     public class DefaultController : Controller
     {
+
         // GET: Default
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TblHakkimda.ToList();
+
+            return View(degerler);
         }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimlerim = db.TblEgitimlerim.ToList();
+            return PartialView(egitimlerim);
+        }
+
+        public PartialViewResult Yeteneklerim()
+        {
+            var yeteneklerim = db.TblYeteneklerim.ToList();
+            return PartialView(yeteneklerim);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobilerim = db.TblHobilerim.ToList();
+            return PartialView(hobilerim);
+        }
+
+        public PartialViewResult Sertifikalarim()
+        {
+            var sertifikalarım = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalarım);
+        }
+
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult iletisim(Tbliletisim t)
+        {
+            db.Tbliletisim.Add(t);
+            t.Tarih = DateTime.Parse( DateTime.Now.ToShortDateString());
+            db.SaveChanges();
+            return PartialView();
+        }
+
     }
 }
